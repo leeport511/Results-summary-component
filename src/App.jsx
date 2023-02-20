c60d18ac9027698177a6826115e84252b2956ba3
leeport511 <leonardoandres.portillo@gmail.com>
2023-02-20 20:07:43 -0300

    done with mobile desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,72 @@
 import { data } from "./data/data";
-
-console.log(data[0].category);
+import "./assets/fonts/HankenGrotesk-VariableFont_wght.ttf";
 
 function App() {
     return (
-        <div>
-            <section>
-                <h4>Your Result</h4>
-                <div>
-                    <p>76</p>
-                    <p>of 100</p>
+        <div className="container mx-auto h-screen">
+            <section className="rounded-b-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue p-4 text-center">
+                <h4 className="mt-3 text-lg font-medium text-gray-300">
+                    Your Result
+                </h4>
+                <div className="mt-5 inline-block">
+                    <div className="h-40 w-40 rounded-full bg-gradient-to-b from-violet-blue to-persian-blue pt-8">
+                        <p className="mb-3 text-6xl font-extrabold text-white">
+                            76
+                        </p>
+                        <p className="text-base font-medium text-gray-400">
+                            of 100
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <h3>Great</h3>
-                    <p>
-                        You scored higher than 65% of the people who have taken
-                        these tests.
+                <div className="mt-5 p-4">
+                    <h3 className="mb-2 text-2xl font-bold text-white">
+                        Great
+                    </h3>
+                    <p className="text-base font-medium text-gray-400">
+                        You scored higher than 65% of the
+                        <br />
+                        people who have taken these tests.
                     </p>
                 </div>
             </section>
-            <section>
-                <h3>Summary</h3>
+            <section className="p-6">
+                <h3 className="mb-5 text-lg font-bold text-dark-gray-blue">
+                    Summary
+                </h3>
                 {data.map((singleData, i) => {
                     return (
-                        <div key={i}>
-                            <img
-                                src={singleData.icon}
-                                alt={singleData.category}
-                            />
-                            <p>{singleData.category}</p>
-                            <p>{singleData.score} / 100</p>
+                        <div
+                            key={i}
+                            className={`mt-4 flex justify-between rounded-lg p-4 ${singleData.bgcolor} bg-opacity-10`}
+                        >
+                            <div className="flex gap-3 ">
+                                <img
+                                    src={singleData.icon}
+                                    alt={singleData.category}
+                                />
+                                <p className={`${singleData.color} font-bold`}>
+                                    {singleData.category}
+                                </p>
+                            </div>
+                            <div className="flex">
+                                <p className="font-bold">
+                                    {singleData.score}
+                                    &nbsp;
+                                </p>
+                                <p className="font-bold text-light-lavender">
+                                    {" "}
+                                    / 100
+                                </p>
+                            </div>
                         </div>
                     );
                 })}
+                <div className="mt-8 text-center">
+                    <button className=" w-[100%] rounded-full bg-dark-gray-blue from-light-slate-blue to-light-royal-blue p-4 font-medium tracking-wider text-white hover:bg-gradient-to-b">
+                        Continue
+                    </button>
+                </div>
             </section>
-            <div>
-                <button>Continue</button>
-            </div>
         </div>
     );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                //gradients 
+                "light-slate-blue": "hsl(252, 100%, 67%)", //background
+                "light-royal-blue": "hsl(241, 81%, 54%)", //background
+                "violet-blue": "hsla(256, 72%, 46%, 1)", // circle
+                "persian-blue": "hsla(241, 72%, 46%, 0)", // circle
+                //primary
+                "light-red": "hsl(0, 100%, 67%)",
+                "orangey-yellow": "hsl(39, 100%, 56%)",
+                "green-teal": "hsl(166, 100%, 37%)",
+                "cobalt-blue": "hsl(234, 85%, 45%)",
+                //neutral
+                "pale-blue": "hsl(221, 100%, 96%)",
+                "light-lavender": "hsl(241, 100%, 89%)",
+                "dark-gray-blue": "hsl(224, 30%, 27%)",
+            },
+            fontFamily: {
+              HankenGrotesk: ['HankenGrotesk', 'sans-serif']
+            }
+        },
+    },
+    plugins: [],
+};
