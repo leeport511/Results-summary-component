e7c95680626e7fcbe8ec8986566589aaaaa70b7b
leeport511 <leonardoandres.portillo@gmail.com>
2023-02-21 16:44:53 -0300

    remove src from img path

--- src/data/data.js
+++ src/data/data.js
@@ -2,28 +2,28 @@ export const data = [
     {
       "category": "Reaction",
       "score": 80,
-      "icon": "./src/assets/images/icon-reaction.svg",
+      "icon": "./assets/images/icon-reaction.svg",
       "bgcolor": "bg-light-red",
       "color": "text-light-red"
     },
     {
       "category": "Memory",
       "score": 92,
-      "icon": "./src/assets/images/icon-memory.svg",
+      "icon": "./assets/images/icon-memory.svg",
       "bgcolor": "bg-orangey-yellow",
       "color": "text-orangey-yellow"
     },
     {
       "category": "Verbal",
       "score": 61,
-      "icon": "./src/assets/images/icon-verbal.svg",
+      "icon": "./assets/images/icon-verbal.svg",
       "bgcolor": "bg-green-teal",
       "color": "text-green-teal"
     },
     {
       "category": "Visual",
       "score": 72,
-      "icon": "./src/assets/images/icon-visual.svg",
+      "icon": "./assets/images/icon-visual.svg",
       "bgcolor": "bg-cobalt-blue",
       "color": "text-cobalt-blue"
     }
