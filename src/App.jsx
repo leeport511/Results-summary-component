efd0db5aed55f52394dc984a6068a77fc265387a
leeport511 <leonardoandres.portillo@gmail.com>
2023-02-21 15:44:54 -0300

    adjust fontsize section 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
         <div className="container mx-auto h-screen xl:flex xl:items-center xl:justify-center">
             <div className="xl:flex xl:h-[50%] xl:items-center xl:justify-center xl:rounded-3xl xl:shadow-xl xl:shadow-light-lavender xl:w-[50%]">
                 <section className="items-center justify-center rounded-b-3xl bg-gradient-to-b from-light-slate-blue to-light-royal-blue p-4 text-center xl:flex xl:h-[100%] xl:w-[100%] xl:flex-col xl:rounded-3xl">
-                    <h4 className="mt-3 text-lg font-medium text-gray-300">
+                    <h4 className="mt-3 text-xl font-medium text-gray-300">
                         Your Result
                     </h4>
                     <div className="mt-5 inline-block">
@@ -20,10 +20,10 @@ function App() {
                         </div>
                     </div>
                     <div className="mt-5 p-4">
-                        <h3 className="mb-2 text-2xl font-bold text-white">
+                        <h3 className="mb-2 text-3xl font-bold text-white">
                             Great
                         </h3>
-                        <p className="text-base font-medium text-gray-400">
+                        <p className="text-lg font-medium text-gray-400">
                             You scored higher than 65% of the
                             <br />
                             people who have taken these tests.
